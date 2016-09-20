dfdb84c334b9040590f3ba8196f72546fb10fd78
fengqiyue <mynameiszhangxu@gmail.com>
2016-09-20 16:47:50 +0800

    主体完成了，细节还有坑

--- scrollpage.js
+++ scrollpage.js
@@ -1,50 +1,67 @@
 var pageView = document.getElementById('page1');
 
+var fullpage = document.getElementById('fullpage')
+
 var pageHeight = pageView.offsetHeight; //screen.availHeight
 
+var fullpageHeight =fullpage.offsetHeight; 
+var a = 0;
 
 // 翻页事件：向上翻
 function pageUp() {
   console.log(pageYOffset);
-  window.scrollTo(0, pageYOffset - pageHeight);
-  requestAnimationFrame(move);
+  // window.scrollTo(0, pageYOffset - pageHeight);
+  if(1){
+    a = pageHeight + a ;
+    fullpage.style.webkitTransform = "translate3d(0px,"+a+"px,0px)";
+    fullpage.style.transition = "all 700ms ease";  
+  }
 };
 
-// 翻页事件：向下翻
-// function pageDown() {
-//   window.scrollTo(0, pageYOffset + pageHeight);
-// };
+//翻页事件：向下翻
+function pageDown() {
+  // window.scrollTo(0, pageYOffset + pageHeight);
+  if(1){
+    a = a - pageHeight;
+    console.log("pageYOffset="+pageYOffset);
+    console.log("pageHeight="+pageHeight);
+    console.log("a="+a);
+    fullpage.style.webkitTransform = "translate3d(0px,"+a+"px,0px)";
+    fullpage.style.transition = "all 700ms ease";
+    console.log("fullpageHeight=" + fullpageHeight); 
+  }
+};
 
 
 
 
-function  st(args){
-        var  win = window;
-        var  to = args;
-        var  scrollTop = 0;
-        var  step;
+// function  st(args){
+//         var  win = window;
+//         var  to = args;
+//         var  scrollTop = 0;
+//         var  step;
 
-        //兼容所有的浏览器
-        window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || 
-                                        window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+//         //兼容所有的浏览器
+//         window.requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || 
+//                                         window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
 
-        if(document.documentElement&&document.documentElement.scrollTop){
-                scrollTop=document.documentElement.scrollTop;
-        }else  if(document.body){
-                scrollTop=document.body.scrollTop;
-        }
+//         if(document.documentElement&&document.documentElement.scrollTop){
+//                 scrollTop=document.documentElement.scrollTop;
+//         }else  if(document.body){
+//                 scrollTop=document.body.scrollTop;
+//         }
 
-        step = (to-scrollTop)/20;
+//         step = (to-scrollTop)/20;
 
-        function move(){
-                scrollTop += step;
-                window.scrollTo(0,scrollTop);
-                if(step>0&&scrollTop<=to  ||  step<0&&scrollTop>to){
-                      requestAnimationFrame(move);
-                }
-        }
-        requestAnimationFrame(move);
-}
+//         function move(){
+//                 scrollTop += step;
+//                 window.scrollTo(0,scrollTop);
+//                 if(step>0&&scrollTop<=to  ||  step<0&&scrollTop>to){
+//                       requestAnimationFrame(move);
+//                 }
+//         }
+//         requestAnimationFrame(move);
+// }
 
 
 
